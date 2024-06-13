b160fda020196d9c039605779b803c95186d40e7
山下泰征 <taisei@MacBook-Pro-152.local>
2024-06-13 14:57:32 +0900

    着信音できた

--- src/my_test.c
+++ src/my_test.c
@@ -9,15 +9,63 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#include <termios.h>
+#include <fcntl.h>
+
+# define max_call 3
+
+
+/************************************************************
+************************************************************/
+
+/******************************
+******************************/
+int kbhit(void)
+{
+	struct termios oldt, newt;
+	int ch;
+	int oldf;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+	ch = getchar();
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+	if (ch != EOF) {
+		ungetc(ch, stdin);
+		return 1;
+	}
+
+	return 0;
+}
+
+int counter = 0;
+
+// 0なら電話する、1なら電話しない
+int flag = 0
+
 int main(){
-    for (int i = 0; i < 3; ++i){
+    while(!kbhit() && counter < max_call){
         FILE *fp;
-            char *cmdline = "play ../data/Ringtone/call.mp3 ";
-            if((fp = popen(cmdline, "w")) == NULL){
-                perror("popen");
-                exit(1);
-            }
+        char *cmdline = "play ../data/Ringtone/call.mp3 ";
+        if((fp = popen(cmdline, "w")) == NULL){
+            perror("popen");
+            exit(1);
+        }
+        ++counter;
         pclose(fp);
     }
+
+    if (counter == max_call){
+        flag = 1;
+    }
+
     return 0;
 }
